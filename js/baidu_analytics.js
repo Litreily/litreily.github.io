23150c70123db8f286b0a8855fdb95ce0d771780
litreily <707922098@qq.com>
2020-03-09 20:00:41 +0800

    Site updated: 2020-03-09 20:00:30

---
+++ js/baidu_analytics.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?d55250b3059d32736607d30baa6e0ca2";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+
+})();
